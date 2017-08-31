48bcea8252dca0011360c3ace9ebab182d1f17d5
Thomas Leese <thomas@leese.io>
2017-08-31 10:35:23 +0100

    Add initial basic code

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask
+import decimal
+from json import JSONEncoder
+import os
+
+from flask import Flask, jsonify
+from tesco import Tesco
+
+
+class MyJSONEncoder(JSONEncoder):
+
+    def default(self, obj):
+        if isinstance(obj, decimal.Decimal):
+            return str(obj)
+        else:
+            return super().default(obj)
 
 
 app = Flask(__name__)
+app.json_encoder = MyJSONEncoder
+
+tesco = Tesco(os.environ['TESCO_API_KEY'])
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/<gtin>')
+def lookup(gtin):
+    product = tesco.lookup(gtin=gtin)[0]
+    return jsonify(product=product)
